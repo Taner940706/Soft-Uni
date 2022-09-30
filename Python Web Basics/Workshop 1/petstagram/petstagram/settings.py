d8e9800f93023010b5436216cc317c4dabbe98f5
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-09-30 05:09:34 -0700

    Workshop 1
    Part Two

--- Python Web Basics/Workshop 1/petstagram/petstagram/settings.py
+++ Python Web Basics/Workshop 1/petstagram/petstagram/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^hv9sx))u4^8%kv8%yewh)i!et5@3i1=ygd9fgcd*azjm4$&p%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -122,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
