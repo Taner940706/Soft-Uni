30115af1a44d22b0f960be1bd7c627c77f06c766
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-10-05 10:53:41 -0700

    Models
    Part Two

--- Python Web Basics/Models 1/djangoProject2/djangoProject2/Web/admin.py
+++ Python Web Basics/Models 1/djangoProject2/djangoProject2/Web/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from djangoProject2.Web.models import Employee
+from djangoProject2.Web.models import Employee, NullBlankDemo
 
 
 # This model is enabled in Django admin
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'review')
+    list_filter = ('first_name')
+    search_fields = ('review')
+
+    fieldsets = (
+        'personal info',
+        {
+            'fields':('first_name', 'review')
+        }
+    )
+
+@admin.register(NullBlankDemo)
+class NullBlankDemo(admin.ModelAdmin):
     pass
--- Python Web Basics/Models 1/djangoProject2/djangoProject2/Web/models.py
+++ Python Web Basics/Models 1/djangoProject2/djangoProject2/Web/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 
+class Department(models.Model):
+    name = models.CharField(max_length=15)
+
 class Employee(models.Model):
     #var char (30) => strings with max length 30
     first_name = models.CharField(max_length=30)
@@ -22,6 +25,35 @@ class Employee(models.Model):
 
     updated_on = models.DateTimeField(
         auto_now=True #optionas
+    )
+    age = models.IntegerField(
+        default=-1,
+        verbose_name='Age of student'
+    )
+
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+    )
+
+
+
+class NullBlankDemo(models.Model):
+    blank = models.IntegerField(
+        blank=True,
+        null = False,
+    )
+    null = models.IntegerField(
+        blank=False,
+        null=True,
+    )
+    blank_null = models.IntegerField(
+        blank=True, #form validation
+        null=True,
+    )
+    default = models.IntegerField(
+        blank=False,
+        null=False,
     )
 
 
