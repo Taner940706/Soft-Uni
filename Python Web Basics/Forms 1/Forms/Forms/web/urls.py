1a4507c16e312b50f0bc6d5ef98711cb8f157407
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-10-15 09:49:47 -0700

    Form 1
    Part Two

--- Python Web Basics/Forms 1/Forms/Forms/web/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from Forms.web.views import index
-
-urlpatterns=(
-    path('', index, name='index'),
-)
--- Python Web Basics/Forms 1/Forms/forms_demos/asgi.py
+++ Python Web Basics/Forms 1/Forms/forms_demos/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Forms project.
+ASGI config for forms_demos project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Forms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forms_demos.settings')
 
 application = get_asgi_application()
