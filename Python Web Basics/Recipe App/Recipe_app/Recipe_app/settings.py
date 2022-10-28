39f639c2afee8d369c211a1251a7e5ce6eaaf6a4
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-10-28 11:01:49 -0700

    Recipe App
    correct image vizualization

--- Python Web Basics/Recipe App/Recipe_app/Recipe_app/settings.py
+++ Python Web Basics/Recipe App/Recipe_app/Recipe_app/settings.py
@@ -113,6 +113,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'staticfiles',)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
